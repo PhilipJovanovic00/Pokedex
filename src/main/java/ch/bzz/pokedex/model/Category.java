58afe21eabb8c31aea71ec7c36b447b9f3400898
Philip Jovanovic <jovanovicp@bzz.ch>
2022-05-11 15:35:11 +0200

    Added Model Classes.

--- src/main/java/ch/bzz/pokedex/model/Category.java
+++ src/main/java/ch/bzz/pokedex/model/Category.java
@@ -1,4 +1,34 @@
 package ch.bzz.pokedex.model;
 
+import java.util.List;
+
 public class Category {
+
+    private List<Category> categories;
+    private int categoryId;
+    private String categoryName;
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
